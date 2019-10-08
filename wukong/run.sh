249479ad4112b8be55b27810a6077892aea007d1
PatrickChina <33338982+PatrickChina@users.noreply.github.com>
2019-10-09 07:30:54 +0800

    Update run.sh

--- wukong/run.sh
+++ wukong/run.sh
@@ -1,4 +1,4 @@
 alias cp='cp'
-wget --user=hassio --password='wukong2019hassio' ftp://192.168.1.229/share/wukongdata/config.yml
+wget --user=hassio --password='wukong2019hassio' ftp://127.0.0.1/share/wukongdata/config.yml
 cp -f config.yml /root/.wukong/config.yml
 python3 wukong.py
